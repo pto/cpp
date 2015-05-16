a2758cebbae25a5239c8a51b0d1a4890e1f03ace
Peter Olsen <pto@me.com>
2015-05-16 18:59:13 -0400

    clean up tabs

--- ch02/p0040a.cpp
+++ ch02/p0040a.cpp
@@ -2,8 +2,9 @@
 
 int main()
 {
-    std::cout << "sizeof(char) is " << sizeof(char) << "\n";
-	std::cout << "sizeof(bool) is " << sizeof(bool) << "\n";
-    std::cout << "sizeof(int) is  " << sizeof(int) << "\n";
+	std::cout << "sizeof(bool) is   " << sizeof(bool) << "\n";
+	std::cout << "sizeof(char) is   " << sizeof(char) << "\n";
+	std::cout << "sizeof(int) is    " << sizeof(int) << "\n";
 	std::cout << "sizeof(double) is " << sizeof(double) << "\n";
+	std::cout << "sizeof(long) is   " << sizeof(long) << "\n";
 }
