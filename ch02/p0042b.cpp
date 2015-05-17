e2c8b3807b7e18fae018886c033ac0558d5034ad
Peter Olsen <pto@me.com>
2015-05-16 20:04:43 -0400

    standize output and spacing

--- ch02/p0042b.cpp
+++ ch02/p0042b.cpp
@@ -12,24 +12,24 @@ double sum(const vector<double>& v)
 {
     double sum = 0.0;
     for (double d : v)
-		sum += d;
+        sum += d;
     return sum;
 }
 
 int main()
 {
     const int dmv = 17;
-	cout << "17: " << dmv << "\n";
+    cout << "dmv: " << dmv << "\n";
     int var = 17;
-	cout << "17: " << var << "\n";
+    cout << "var: " << var << "\n";
     constexpr double max1 = 1.4 * square(dmv);
-    cout << "404.6: " << max1 << "\n";
+    cout << "max1: " << max1 << "\n";
     // constexpr double max2 = 1.4 * square(var); // error: constexpr of var
     const double max3 = 1.4 * square(var);
-    cout << "404.6: " << max3 << "\n";
+    cout << "max3: " << max3 << "\n";
     
     vector<double> v {1.2, 3.4, 4.5};
     const double s1 = sum(v);
-    cout << "9.1: " << s1 << "\n";
-    // constexpr double s2 = sum(v); // error: no constexpr function
+    cout << "s1: " << s1 << "\n";
+    // constexpr double s2 = sum(v); // error: not constexpr function
 }
