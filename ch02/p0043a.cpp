462b3841e38b66c1bac449714845b717d920ec43
Peter Olsen <pto@me.com>
2015-05-16 20:12:10 -0400

    cleanup tabs

--- ch02/p0043a.cpp
+++ ch02/p0043a.cpp
@@ -3,64 +3,64 @@ using namespace std;
 
 bool accept()
 {
-	cout << "Do you want to proceed (y or n)?\n";
+    cout << "Do you want to proceed (y or n)?\n";
 
-	char answer = 0;
-	cin >> answer;
+    char answer = 0;
+    cin >> answer;
 
-	if (answer == 'y') return true;
-	return false;
+    if (answer == 'y') return true;
+    return false;
 }
 
 bool accept2()
 {
-	cout << "Do you want to proceed (y or n)?\n";
+    cout << "Do you want to proceed (y or n)?\n";
 
-	char answer = 0;
-	cin >> answer;
+    char answer = 0;
+    cin >> answer;
 
-	switch (answer) {
-	case 'y':
-		return true;
-	case 'n':
-		return false;
-	default:
-		cout << "I'll take that for a no.\n";
-		return false;
-	}
+    switch (answer) {
+    case 'y':
+        return true;
+    case 'n':
+        return false;
+    default:
+        cout << "I'll take that for a no.\n";
+        return false;
+    }
 }
 
 bool accept3()
 {
-	int tries = 1;
-	while (tries < 4) {
-		cout << "Do you want to proceed (y or n)?\n";
+    int tries = 1;
+    while (tries < 4) {
+        cout << "Do you want to proceed (y or n)?\n";
 
-		char answer = 0;
-		cin >> answer;
+        char answer = 0;
+        cin >> answer;
 
-		switch (answer) {
-		case 'y':
-			return true;
-		case 'n':
-			return false;
-		default:
-			cout << "Sorry, I don't understand that.\n";
-			++tries;
-		}
-	}
-	cout << "I'll take that for a no.\n";
-	return false;
+        switch (answer) {
+        case 'y':
+            return true;
+        case 'n':
+            return false;
+        default:
+            cout << "Sorry, I don't understand that.\n";
+            ++tries;
+        }
+    }
+    cout << "I'll take that for a no.\n";
+    return false;
 }
 
 int main()
 {
-	bool result = accept();
-	cout << "The result of accept() is " << result << "\n";
+    bool result = accept();
+    cout << "The result of accept() is " << result << "\n";
 
-	result = accept2();
-	cout << "The result of accept2() is " << result << "\n";
+    result = accept2();
+    cout << "The result of accept2() is " << result << "\n";
 
-	result = accept3();
-	cout << "The result of accept3() is " << result << "\n";
+    result = accept3();
+    cout << "The result of accept3() is " << result << "\n";
 }
